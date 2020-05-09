09abf763c000c23e3d0330db8b2082b2c5ea7562
ashish21.kumar <ashish21.kumar@lge.com>
2020-05-09 17:04:46 +0530

    picture test

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
+const path = require('path')
 const app = express();
 const hbs = require('hbs');
 const port = process.env.PORT||3000;
-
+const publicDirectoryPath = path.join(__dirname, './public')
 
 app.set('view engine', 'hbs')
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('/',(req,res) =>{
     res.render('index');
